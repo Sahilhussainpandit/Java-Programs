a0a49f74c5baa03519936e0f88fe79228fc58bd1
Sahilhussainpandit <90780191+Sahilhussainpandit@users.noreply.github.com>
2021-12-04 15:26:04 +0530

    Add files via upload

---
+++ CompareTwoNumbers.java
@@ -0,0 +1,19 @@
+ 
+public class CompareTwoNumbers {
+ 
+ public static void main(String[] ar) {
+ 
+ int num1 = 3;
+ int num2 = 6;
+ 
+ if(num1 > num2){
+ System.out.println(num1 + " is greater than " + num2);
+ }
+ else if(num1 < num2){
+ System.out.println(num1 + " is less than " + num2);
+ }
+ else{
+ System.out.println(num1 + " is equal to " + num2);
+ }
+ }
+}
